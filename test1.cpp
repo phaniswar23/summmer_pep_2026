5016628e755e12e1af0f3b9b7265c767a8f74fd8
Phaniswar <phaniswarjanyavula@gmail.com>
2026-06-22 16:25:05 +0530

    Auto update on save

--- test1.cpp
+++ test1.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main(){
     char grade = 'A';
+    switch (expression)
+    {
+    case constant expression:
+        /* code */
+        break;
     
+    default:
+        break;
+    }
     return 0;
 }
