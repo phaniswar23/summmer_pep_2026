c4e7e65ab2b06f85eacc59927e6c41564ac16447
Phaniswar <phaniswarjanyavula@gmail.com>
2026-06-22 16:49:25 +0530

    Auto update on save

--- Pattern.cpp
+++ Pattern.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     for (id_t i = 0; i < 5; i++)
     {
-        for (int j = 0; j < 6; j++)
+        for (int j = 0; j < 5; j++)
         {
             cout << "*" << " " << endl;
         }
