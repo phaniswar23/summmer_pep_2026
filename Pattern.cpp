a2eaeacf72ddc028996e5b4b724b55054ec5593c
Phaniswar <phaniswarjanyavula@gmail.com>
2026-06-22 16:46:46 +0530

    Auto update on save

--- Pattern.cpp
+++ Pattern.cpp
@@ -4,13 +4,7 @@ using namespace std;
 int main(){
     for (id_t i = 0; i < 6; i++)
     {
-        for (int <?php
-        echo ;
-        ?> = 0; <?php
-        echo ;
-        ?> < count; <?php
-        echo ;
-        ?>++)
+        for (int j = 0; j < count; j++)
         {
             /* code */
         }
