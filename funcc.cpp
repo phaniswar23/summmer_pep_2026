ef465ffb9d94bb2e3ce4893fcbe707bec5673a74
Phaniswar <phaniswarjanyavula@gmail.com>
2026-06-23 14:51:55 +0530

    23-06-2026 14:51:55

---
+++ funcc.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void func1()
+{
+    cout << "This is Void Function" << endl;
+    
+}
+void doubleThevalue(int &num)
+{
+    num *= 2;
+    cout << "Inside: " << num << endl;
+    
+}
+
+int main(){
+    int num = 10;
+    doubleThevalue(num);
+    cout << "OutSide: " << num << endl;
+    
+    return 0;
+}
