54b7a13bfe6be708643ca854c2c38069a978d563
Phaniswar <phaniswarjanyavula@gmail.com>
2026-06-23 16:03:13 +0530

    23-06-2026 16:03:13

--- DSA/Arrays/vector.cpp
+++ DSA/Arrays/vector.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(){
     vector<int> vec1 = {1,2,3,4,5};
     cout << vec1.at(3) << endl;
-    cout << vec1.at(3) << endl;
+    cout << vec1.push_back(3) << endl;
     return 0;
 }
