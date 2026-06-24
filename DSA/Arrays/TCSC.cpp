08f6bef7c2141bec2840fb573956194427fd22bd
Phaniswar <phaniswarjanyavula@gmail.com>
2026-06-24 15:56:16 +0530

    24-06-2026 15:56:16

--- DSA/Arrays/TCSC.cpp
+++ DSA/Arrays/TCSC.cpp
@@ -1,4 +1,4 @@
 /*
 Time Complexity - How much time an algorithm takes as input size increases
-Big O - Represets Upper Bound of running time,it gives the worst case scenerio for an alo
+Big O - Represets Upper Bound of running time,it gives the worst case scenerio for an algor
 */
