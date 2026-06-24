de1f1c0f56da993de7cf67637b2a4e2d0112852d
Phaniswar <phaniswarjanyavula@gmail.com>
2026-06-24 15:41:33 +0530

    24-06-2026 15:41:33

--- DSA/Arrays/LinearSearch.cpp
+++ DSA/Arrays/LinearSearch.cpp
@@ -45,6 +45,6 @@ int main(){
     int key = 53;
     int n = sizeof(arr) / sizeof(arr[0]);
    cout<< LinearSearch(arr,key) << endl;
-   cout<< BinarySearch(arr,n,)
+   cout<< BinarySearch(arr,n,ke)
     return 0;
 }
