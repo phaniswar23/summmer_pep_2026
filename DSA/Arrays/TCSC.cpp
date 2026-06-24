5837ed272fc8e907478436a13eec700a0c10f6c1
Phaniswar <phaniswarjanyavula@gmail.com>
2026-06-24 16:10:48 +0530

    24-06-2026 16:10:48

--- DSA/Arrays/TCSC.cpp
+++ DSA/Arrays/TCSC.cpp
@@ -11,7 +11,7 @@ O(log n) => Logarathemic time -> The time grows logaramthmically as input size i
 
 O(n log n) => Linearithemic -> Mix of Linear and Logarathmic,example :- Quick sort and Merge Sort
 
-O(n^2) => Quadratic -> Time will  grow proportinally 
+O(n^2) => Quadratic -> Time will  grow proportinally to 
 
 
 One loop              → O(n)
