cbc7da13fe3ffad141fc9c64b1911e059d03ea15
Phaniswar <phaniswarjanyavula@gmail.com>
2026-06-24 15:58:35 +0530

    24-06-2026 15:58:35

--- DSA/Arrays/TCSC.cpp
+++ DSA/Arrays/TCSC.cpp
@@ -2,5 +2,5 @@
 Time Complexity - How much time an algorithm takes as input size increases
 Big O - Represets Upper Bound of running time,it gives the worst case scenerio for an algorithm,it describes the maximum
 
-O(1) => Constant time -> The times taken donesn't depand on the input size,algorithm always run in 
+O(1) => Constant time -> The times taken donesn't depand on the input size,algorithm always run in the same time
 */
