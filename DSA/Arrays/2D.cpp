a6d880acf58ee910d0dcdb6cda7f95079e0ca6a6
Phaniswar <phaniswarjanyavula@gmail.com>
2026-06-30 16:44:59 +0530

    30-06-2026 16:44:59

--- DSA/Arrays/2D.cpp
+++ DSA/Arrays/2D.cpp
@@ -18,15 +18,15 @@ void printevencol(int arr[3][4])
 int main(){
     
     int arr[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
-    cout<<"Enter values of arr" << endl;
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            cin >> arr[i][j];
-        }
+    // cout<<"Enter values of arr" << endl;
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 4; j++)
+    //     {
+    //         cin >> arr[i][j];
+    //     }
         
-    }
+    // }
 
     cout << "Output" << endl;
     printevencol(arr);
