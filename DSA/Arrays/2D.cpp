e8eb532a0028c95723190d717dd4de8a9ed98b17
Phaniswar <phaniswarjanyavula@gmail.com>
2026-06-30 16:32:27 +0530

    30-06-2026 16:32:27

--- DSA/Arrays/2D.cpp
+++ DSA/Arrays/2D.cpp
@@ -18,7 +18,7 @@ int main(){
     {
         for (int j = 0; j < 4; j++)
         {
-            cout << arr[i][j] << "/n" << endl;
+            cout << arr[i][j] << /n" << endl;
         }
         
     }
