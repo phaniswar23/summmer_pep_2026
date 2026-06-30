30577b81141cc9d67a11be0365e9c5e1d59e95f6
Phaniswar <phaniswarjanyavula@gmail.com>
2026-06-30 16:31:44 +0530

    30-06-2026 16:31:44

--- DSA/Arrays/2D.cpp
+++ DSA/Arrays/2D.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     
     int arr[3][4];
-    cout<<"Enter values of arr" << 
+    cout<<"Enter values of arr" << endl;
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 4; j++)
