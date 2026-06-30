5382b59edcc95c7ff4bfc65e869f8e7a1aa84f8c
Phaniswar <phaniswarjanyavula@gmail.com>
2026-06-30 15:37:56 +0530

    30-06-2026 15:37:56

--- DSA/Arrays/Recursion.cpp
+++ DSA/Arrays/Recursion.cpp
@@ -17,7 +17,7 @@ bool LinearSearch(int arr[],int size,int key)
 {
     if(arr[size] == key) return true;
 
-    return LinearSearch(arr)
+    return LinearSearch(arr + 1,)
 }
 int main(){
   cout <<  fact(6) << endl;
