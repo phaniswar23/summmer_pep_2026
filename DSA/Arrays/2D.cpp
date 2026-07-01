1f612551f3180c0ee69bbe4e7dfdc8f28b622acf
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-01 14:38:50 +0530

    01-07-2026 14:38:50

--- DSA/Arrays/2D.cpp
+++ DSA/Arrays/2D.cpp
@@ -19,9 +19,10 @@ bool search(int arr[][3],int row,int col,int target)
 //Calculate and print sum of every row
 void calprint(int arr[3][3])
 {
-    int sum = 0;
+   
     for (int i = 0; i < 3; i++)
     {
+         int sum = 0;
         for (int j = 0; j < 3; j++)
         {
             
