510077d69ec096778e1defd21c9d70d275f331f5
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-01 15:17:50 +0530

    01-07-2026 15:17:50

--- DSA/Arrays/2D.cpp
+++ DSA/Arrays/2D.cpp
@@ -65,7 +65,7 @@ void shape(int arr[][4])
             }
             else
             {
-                for(int i = 4;i > 0;i--)
+                for(int i = 4;i >= 0;i--)
             {
                 cout << arr[i][j] << " ";
             }
