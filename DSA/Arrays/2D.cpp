5623dcbf48daf86722375421829cf8838bdf7279
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-01 14:42:05 +0530

    01-07-2026 14:42:05

--- DSA/Arrays/2D.cpp
+++ DSA/Arrays/2D.cpp
@@ -32,7 +32,20 @@ void calprint(int arr[3][3])
     
 }
 //print thr value of max sum and also its index
+void calprint(int arr[3][3])
+{
+   
+    for (int i = 0; i < 3; i++){
+    int sum = 0;
+        for (int j = 0; j < 3; j++)
+        {
+            sum += arr[i][j];
+        }
+        cout << sum << endl;
+    }
 
+    
+}
 int main(){
     
     int arr[3][3] = {1,2,3,4,5,6,7,8,9};
