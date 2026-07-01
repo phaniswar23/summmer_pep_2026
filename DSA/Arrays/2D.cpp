586eadd75cfeec2945a2d6244ce9abf2cf4d1686
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-01 14:50:42 +0530

    01-07-2026 14:50:42

--- DSA/Arrays/2D.cpp
+++ DSA/Arrays/2D.cpp
@@ -50,15 +50,6 @@ void maxprint(int arr[3][3])
 int main(){
     
     int arr[3][3] = {1,2,3,7,8,9,7,8,9};
-    // cout<<"Enter values of arr" << endl;
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     for (int j = 0; j < 4; j++)
-    //     {
-    //         cin >> arr[i][j];
-    //     }
-        
-    // }
 
         calprint(arr);
         maxprint(arr);
