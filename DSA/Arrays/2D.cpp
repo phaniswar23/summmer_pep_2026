4212fa5e60b343c2df9282a43457c17773ef20e2
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-01 14:43:07 +0530

    01-07-2026 14:43:07

--- DSA/Arrays/2D.cpp
+++ DSA/Arrays/2D.cpp
@@ -41,8 +41,7 @@ void maxprint(int arr[3][3])
         {
             sum += arr[i][j];
         }
-        int maxi = 0;
-        maxi = max(maxi,sum);
+        
         cout << sum << endl;
         cout << maxi << endl;
     }
