925f310495639997134bc7f30280cee2331713b0
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-01 15:50:13 +0530

    01-07-2026 15:50:13

--- DSA/Arrays/2D.cpp
+++ DSA/Arrays/2D.cpp
@@ -79,13 +79,14 @@ void spiralprint(int arr2[4][4], int row, int col)
 
     while(top <= bottom && left <= right)
     {
+        
         for (int i = left; i <= right; i++)
         {
             cout << arr2[top][i] << endl;
         }
         top++;
 
-        
+
         
     }
 }
