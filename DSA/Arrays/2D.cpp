203ce31564a3f92f38cb904c19cd52c3d4d985c1
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-01 14:35:00 +0530

    01-07-2026 14:35:00

--- DSA/Arrays/2D.cpp
+++ DSA/Arrays/2D.cpp
@@ -22,7 +22,7 @@ void calprint(int arr[3][3])
     int sum = 0;
     for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = i; j < 3; j++)
         {
             
             sum += arr[i][j];
