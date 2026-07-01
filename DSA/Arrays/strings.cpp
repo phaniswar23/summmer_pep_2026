c9d9289af3e7c979634d33c2308ef4c7d0617f8f
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-01 16:21:07 +0530

    01-07-2026 16:21:07

--- DSA/Arrays/strings.cpp
+++ DSA/Arrays/strings.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
     string str1 = "Hello";
     string str2 = "hello";
-    if(str1 == str2) cout << "SAME" << w
+    if(str1 == str2) cout << "SAME" << endl;
+    
     return 0;
 }
