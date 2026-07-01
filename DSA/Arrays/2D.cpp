d52e09097bbb1fd71738127f53ff1bd24ea4a712
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-01 15:46:53 +0530

    01-07-2026 15:46:53

--- DSA/Arrays/2D.cpp
+++ DSA/Arrays/2D.cpp
@@ -114,7 +114,7 @@ int main()
         {3, 16, 9, 7}};
     // calprint(arr);
     // maxprint(arr);
-   cout<< "Wave Shape: " << s << endl;
+   cout<< "Wave Shape: " <<  << endl;
    cout  spiralprint(arr2);
     return 0;
 }
