30fc4c539e307ee3ee1159dd8e99753f6d47ea79
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-01 14:40:52 +0530

    01-07-2026 14:40:52

--- DSA/Arrays/2D.cpp
+++ DSA/Arrays/2D.cpp
@@ -20,7 +20,7 @@ bool search(int arr[][3],int row,int col,int target)
 void calprint(int arr[3][3])
 {
    
-    for (int i = 0; i < 3; i++){}
+    for (int i = 0; i < 3; i++){
     int sum = 0;
         for (int j = 0; j < 3; j++)
         {
