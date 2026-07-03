bdecb23337965bfba02fbc124ec41c25bd98b3a6
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-03 16:35:15 +0530

    03-07-2026 16:35:15

--- DSA/Arrays/LinkedList.cpp
+++ DSA/Arrays/LinkedList.cpp
@@ -41,7 +41,8 @@ void printevenlist(Node* head)
     Node* temp = head;
     while (temp != NULL)
     {
-        cout << temp->data << "->"
+        cout << temp->data << "->";
+        temp = temp->next;
     }
     
     
