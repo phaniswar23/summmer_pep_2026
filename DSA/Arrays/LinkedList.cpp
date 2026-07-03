ee2cdb603e9d22cebee71dde3b9584517f946e93
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-03 16:50:56 +0530

    03-07-2026 16:50:56

--- DSA/Arrays/LinkedList.cpp
+++ DSA/Arrays/LinkedList.cpp
@@ -50,6 +50,7 @@ void insertAtPosition(Node* &head,int pos,int val)
 {
     Node* node = new Node(val);
     Node* temp = head;
+    
     for (int i = 1; i < pos - 1 && temp != NULL; i++)
     {
         temp = temp->next;
