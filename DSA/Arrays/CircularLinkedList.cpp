6417ff07ce5baacba010e1eb6b7b4f2d7b42bb60
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-06 15:41:11 +0530

    06-07-2026 15:41:11

--- DSA/Arrays/CircularLinkedList.cpp
+++ DSA/Arrays/CircularLinkedList.cpp
@@ -17,7 +17,7 @@ void insertAtEnd(Node* &head,int val)
 {
     Node* newNode = new Node(val);
     Node* temp = head;
-    while (temp->head != )
+    while (temp->head != head)
     {
         /* code */
     }
