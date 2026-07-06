91e058746011102771ea7af44222faacef142d72
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-06 14:53:37 +0530

    06-07-2026 14:53:37

--- DSA/Arrays/LinkedList.cpp
+++ DSA/Arrays/LinkedList.cpp
@@ -156,7 +156,7 @@ int main()
     cout << "After Delete Last Node: ";
     printList(head);
     
-    deleteAtBeg(Node* &head);
+    deleteAtBeg(head);
 
     cout << "After Delete First Node: ";
     printList(head);
