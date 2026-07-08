809f8d23d28e7094923ad390f9967e754369b4b5
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-08 15:27:12 +0530

    08-07-2026 15:27:12

--- DSA/Arrays/FloydLoop.cpp
+++ DSA/Arrays/FloydLoop.cpp
@@ -5,5 +5,5 @@ part - 1 -> detect if loop exits
         fast and slow pointers approach
         init fast and slow at head
         move slow by 1 step and fast by 2 steps until fast becomes equal to null or fast next is null
-        if fast 
+        if fast and slow 
 */
