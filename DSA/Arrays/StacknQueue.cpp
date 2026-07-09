fb27f34aa9776f58b3a6e4c0b4d24e66f5a85537
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-09 16:34:20 +0530

    09-07-2026 16:34:20

--- DSA/Arrays/StacknQueue.cpp
+++ DSA/Arrays/StacknQueue.cpp
@@ -7,34 +7,72 @@ public:
     int *arr;
     int top;
     int size;
+
     myStack(int size)
     {
         this->size = size;
         arr = new int[size];
         top = -1;
     }
+
     void push(int val)
     {
         if (top == size - 1)
         {
-            cout << "Stack OverFlow" << endl;
+            cout << "Stack Overflow" << endl;
+            return;
         }
+
         top++;
         arr[top] = val;
     }
-    void pop(int val)
+
+    void pop()
     {
-        if(top == -1)
+        if (top == -1)
         {
-            cout << "Stack UnderFlow" << endl;
+            cout << "Stack Underflow" << endl;
+            return;
         }
+
         top--;
+    }
+
+    int peek()
+    {
+        if (top == -1)
+        {
+            cout << "Stack is Empty" << endl;
+            return -1;
+        }
+
+        return arr[top];
+    }
+
+    bool isEmpty()
+    {
+        return top == -1;
+    }
+
+    bool isFull()
+    {
+        return top == size - 1;
     }
 };
 
 int main()
 {
     myStack s1(5);
-    s1.push(1);
 
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+
+    cout << "Top Element: " << s1.peek() << endl;
+
+    s1.pop();
+
+    cout << "Top Element: " << s1.peek() << endl;
+
+    return 0;
 }
