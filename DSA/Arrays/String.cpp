fa9c91f178e6601c4570194bb5027330a32f2ae0
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-10 16:28:15 +0530

    10-07-2026 16:28:15

--- DSA/Arrays/String.cpp
+++ DSA/Arrays/String.cpp
@@ -3,7 +3,7 @@ using namespace std;
 void reverseString(string str)
 {
     stack<string> st;
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < str.length(); i++)
     {
         /* code */
     }
