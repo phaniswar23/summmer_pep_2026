d6d58216bbd673af3cf1ebe9f058adc9b8ad88c7
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-10 14:07:22 +0530

    10-07-2026 14:07:22

--- DSA/Arrays/Primenumber.cpp
+++ DSA/Arrays/Primenumber.cpp
@@ -5,7 +5,7 @@ bool primeNumber(int n)
 if(n == 2) return true;
     for(int i = 0;i * i <= n;i++)
     {
-        if()
+        if(n % 1 == 0 && n % n == 0)
         {
             return false;
         }
