422a2677cdfedab276c8b4edadcc0cf18e4eff2d
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-10 13:57:19 +0530

    10-07-2026 13:57:19

--- DSA/Arrays/Primenumber.cpp
+++ DSA/Arrays/Primenumber.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 int primeNumber(int n)
 {
-
+if(n == 2) return true;
+    for(int i = 0;i <= n;i++)
+    {
+        if(n % i == 0)
+        {
+            return false;
+        }
+        return true;
+    }
 }
 int main(){
     int n = 7;
