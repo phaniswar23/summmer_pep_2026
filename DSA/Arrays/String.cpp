3d5878cc532bb3667ca76591123f2b8043b6526b
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-10 16:29:13 +0530

    10-07-2026 16:29:13

--- DSA/Arrays/String.cpp
+++ DSA/Arrays/String.cpp
@@ -6,7 +6,7 @@ void reverseString(string str)
     for (int i = 0; i < str.length(); i++)
     {
         string ans = " ";
-        while(str[i] != ' ' && i < )
+        while(str[i] != ' ' && i < str.length())
     }
     
 }
