33705757689737deae8701267a93128df622f800
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-14 15:21:48 +0530

    14-07-2026 15:21:48

--- DSA/Arrays/Evaluation.cpp
+++ DSA/Arrays/Evaluation.cpp
@@ -8,7 +8,7 @@ int prefixEval(string s)
 
     //if(s[i])  is operand ->st.push
     //else if it is operator->create two variables to store two elements
-    //pop them and evalute with operator,then push the resultant
+    //pop them and evalute with operator,then push the resultant back into stac
 }
 
 int main(){
