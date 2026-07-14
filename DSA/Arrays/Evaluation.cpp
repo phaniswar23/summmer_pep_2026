cf94417553ccfe10a99088f87ba7d321213e7c52
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-14 15:21:09 +0530

    14-07-2026 15:21:09

--- DSA/Arrays/Evaluation.cpp
+++ DSA/Arrays/Evaluation.cpp
@@ -7,7 +7,7 @@ int prefixEval(string s)
     //Traverse string come right to left
 
     //if(s[i])  is operand ->st.push
-    //else if it is operator->create twovariables
+    //else if it is operator->create two variables
 }
 
 int main(){
