f164d2f172d94a32dcb7cb34671e005a3124a8cc
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-14 15:32:44 +0530

    14-07-2026 15:32:44

--- DSA/Arrays/Evaluation.cpp
+++ DSA/Arrays/Evaluation.cpp
@@ -16,7 +16,7 @@ int prefixEval(string s)
     int n = s.length();
     for(int i = n - 1;i >= 0;i--)
     {
-        if(s[i] >= '0' )
+        if(s[i] >= '0' && s[i] <= '9')
         {
         st.push(s[i]);
         }
