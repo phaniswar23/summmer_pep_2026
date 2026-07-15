9175bbd3a4a755c1d3f23184fa69bd26d026dbcc
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-15 10:56:01 +0530

    15-07-2026 10:56:01

---
+++ DSA/dsa.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void exchangeSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 4, 3, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    exchangeSort(arr, n);
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
