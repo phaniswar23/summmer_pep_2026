ddcda6ff4c76b444fdf008707b52900010871771
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-15 16:44:43 +0530

    15-07-2026 16:44:43

--- DSA/Arrays/Tree.cpp
+++ DSA/Arrays/Tree.cpp
@@ -18,7 +18,7 @@ void preorder(TreeNode* root)
     if(root == NULL) return;
     cout << root->data << endl;
     preorder(root->left);
-    preorder(root->left);
+    preorder(root->right);
 
 }
 int main(){
