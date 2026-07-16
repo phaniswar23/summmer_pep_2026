26611938ed40951b442792930e2bac0cc63f8e65
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-16 15:28:03 +0530

    16-07-2026 15:28:03

--- DSA/Arrays/Tree.cpp
+++ DSA/Arrays/Tree.cpp
@@ -47,7 +47,7 @@ int main()
     root->right = new TreeNode(3);
 
     root->left->left = new TreeNode(4);
-    root->left->right = new TreeNode(4);
+    root->left->right = new TreeNode(5);
 
     return 0;
 }
