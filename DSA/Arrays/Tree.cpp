70bae42e9f7278b38f9fc14d6f3b9fcd204cd3d6
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-16 16:50:11 +0530

    16-07-2026 16:50:11

--- DSA/Arrays/Tree.cpp
+++ DSA/Arrays/Tree.cpp
@@ -78,7 +78,7 @@ void levelOrder(TreeNode* root)
             if(frontNode->left) q.push(frontNode->left);
             if(frontNode->right) q.push(frontNode->right);
         }
-        else if()
+        else if(!q.empty())
     }
 }
 int main()
