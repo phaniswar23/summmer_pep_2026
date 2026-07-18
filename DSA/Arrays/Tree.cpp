bd62b58e2f6c28e103007a8cc20fee06ea54d841
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-18 15:09:56 +0530

    18-07-2026 15:09:56

--- DSA/Arrays/Tree.cpp
+++ DSA/Arrays/Tree.cpp
@@ -55,8 +55,7 @@ int leafNodes(TreeNode *root)
 {
     if(root == NULL) return 0;
     if(root->left == NULL && root->right == NULL)
-    {
-        
+    {      
         return 1;
     }
     return leafNodes(root->left) + leafNodes(root->right);
