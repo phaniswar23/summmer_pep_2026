6eeaaac3e6e373ca72f59c4aac75d83a974920e0
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-21 15:28:07 +0530

    21-07-2026 15:28:07

--- DSA/Arrays/Graph.cpp
+++ DSA/Arrays/Graph.cpp
@@ -17,7 +17,7 @@ int main(){
         int u, v;
         cin >> u >> v;
         adjList[u].push_back(v);
-        adjList[u].push_back(v);
+        adjList[v].push_back(v);
     }
     
     return 0;
