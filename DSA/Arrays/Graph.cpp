2b49094cdc1d5eaa259fbb9e309f6b8e797b4cf6
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-21 15:29:26 +0530

    21-07-2026 15:29:26

--- DSA/Arrays/Graph.cpp
+++ DSA/Arrays/Graph.cpp
@@ -19,7 +19,7 @@ int main(){
         adjList[u].push_back(v);
         adjList[v].push_back(u); //Write this graph is undirected
     }
-    cout << "Adja" << endl;
+    cout << "Adjacency List" << endl;
     
     return 0;
 }
