fea300870030b6513852694101d8c5ea102be693
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-22 13:54:31 +0530

    22-07-2026 13:54:31

--- DSA/Arrays/Graph.cpp
+++ DSA/Arrays/Graph.cpp
@@ -6,7 +6,7 @@ int main(){
     int n;//no of vertices;
     int m;//no of edges;
 
-    
+    cout<< "Enter the number of vertices: " << " ";
     cin >> n;
     cout<< "Enter the number of edges: " << " ";
     cin >> m;
