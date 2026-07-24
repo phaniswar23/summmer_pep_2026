9625da93807b18646b1b0c911fc3cb67f9758df6
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-24 14:41:03 +0530

    24-07-2026 14:41:03

--- DSA/Arrays/BST.cpp
+++ DSA/Arrays/BST.cpp
@@ -11,7 +11,7 @@ public:
         image[r][c] = newColor;
 
         //top
-        dfs()
+        dfs(r-1,c,)
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int originalColor = image[sr][sc];
