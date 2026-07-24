eb8705dbfaef73f8f83762ecae115cd013c15765
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-24 14:29:29 +0530

    24-07-2026 14:29:29

--- DSA/Arrays/BST.cpp
+++ DSA/Arrays/BST.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int originalColor = image
+        int originalColor = image[sr]
     }
 };
 int main(){
