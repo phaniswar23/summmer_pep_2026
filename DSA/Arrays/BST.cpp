9dc68eb65b56606ce87e5faa5f4cd0df31dfa660
Phaniswar <phaniswarjanyavula@gmail.com>
2026-07-24 14:44:58 +0530

    24-07-2026 14:44:58

--- DSA/Arrays/BST.cpp
+++ DSA/Arrays/BST.cpp
@@ -1,44 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    void dfs(int r,int c,vector<vector<int>>& image,int originalColor,int newColor)
+    void dfs(int r, int c, vector<vector<int>> &image, int originalColor, int newColor)
     {
-         int n = image.size();
-       int m = image[0].size();
-        if(r < 0 || r >= n || c < 0 || c >= m)
-        return;
+        int n = image.size();
+        int m = image[0].size();
+        if (r < 0 || r >= n || c < 0 || c >= m)
+            return;
 
-    if(image[r][c] != originalColor)
-        return;
+        if (image[r][c] != originalColor)
+            return;
 
-      image[r][c] = newColor;
+        image[r][c] = newColor;
 
-        //top
-        dfs(r-1,c,image,originalColor,newColor);
-        //bottom
-        dfs(r+1,c,image,originalColor,newColor);
-        //left
-        dfs(r,c-1,image,originalColor,newColor);
-        //right
-        dfs(r,c+1,image,originalColor,newColor);
-
-
-
+        // top
+        dfs(r - 1, c, image, originalColor, newColor);
+        // bottom
+        dfs(r + 1, c, image, originalColor, newColor);
+        // left
+        dfs(r, c - 1, image, originalColor, newColor);
+        // right
+        dfs(r, c + 1, image, originalColor, newColor);
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+    vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int color)
+    {
         int originalColor = image[sr][sc];
 
-        if(originalColor == color) return image;
+        if (originalColor == color)
+            return image;
 
-        //dfs
-        dfs(sr,sc,image,originalColor,color);
+        // dfs
+        dfs(sr, sc, image, originalColor, color);
 
         return image;
-        
     }
 };
-int main(){
-        
+int main()
+{
+
     return 0;
 }
